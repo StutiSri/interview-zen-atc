32c6d0613ca27aa01b009f61fae3e6f53961426a
Stuti Srivastava <stutisrivastv@gmail.com>
2021-01-10 20:44:30 +0530

    Parentheses Validator - #11

--- src/ParenthesesValidator.java
+++ src/ParenthesesValidator.java
@@ -10,8 +10,15 @@ public class ParenthesesValidator {
             this.size = size;
             top = -1;
         }
+
+        public void push(char data){
+            if(top == size - 1){
+                throw new RuntimeException("Overflow!");
+            }
+            elements[++top] = data;
+        }
     }
-    
+
     public boolean isValid(String expression) {
         return false;
     }
