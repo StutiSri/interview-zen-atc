d1e4246bdc4f47154ade13d70be4af07fc86ea8d
Stuti Srivastava <stutisrivastv@gmail.com>
2021-01-10 20:44:18 +0530

    Parentheses Validator - #10

--- src/ParenthesesValidator.java
+++ src/ParenthesesValidator.java
@@ -1,4 +1,17 @@
 public class ParenthesesValidator {
+    class Stack {
+
+        private final char[] elements;
+        private int size;
+        private int top;
+
+        public Stack(int size) {
+            elements = new char[size];
+            this.size = size;
+            top = -1;
+        }
+    }
+    
     public boolean isValid(String expression) {
         return false;
     }
