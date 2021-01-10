19229c75e2dad31a74662fe328d94399876945bb
Stuti Srivastava <stutisrivastv@gmail.com>
2021-01-10 20:38:01 +0530

    Merge Sorted Arrays - #17

--- test/MergeSortedArraysTest.java
+++ test/MergeSortedArraysTest.java
@@ -3,8 +3,8 @@ import org.junit.jupiter.api.Test;
 class MergeSortedArraysTest {
     @Test
     void shouldMergeSortedArrays() {
-        int[] arr1 = new int[]{};
-        int[] arr2 = new int[]{};
+        int[] arr1 = new int[]{1, 2, 3, 5, 6};
+        int[] arr2 = new int[]{4};
 
         int[] merge = new MergeSortedArrays().merge(arr1, arr2);
         for (int num : merge) {
