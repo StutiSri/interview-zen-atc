35e01e905dc16ae6c9ef745a941fd960be36eaf3
Stuti Srivastava <stutisrivastv@gmail.com>
2021-01-10 20:49:04 +0530

    Parentheses Validator - #24

--- test/ParenthesesValidatorTest.java
+++ test/ParenthesesValidatorTest.java
@@ -3,48 +3,56 @@ import org.junit.jupiter.api.Test;
 class ParenthesesValidatorTest {
     @Test
     void test1() {
-        String expression = "[]";
-        boolean valid = new ParenthesesValidator().isValid(expression);
+        boolean valid = new ParenthesesValidator().isValid("[]");
         assert valid;
         System.out.println(valid);
     }
 
     @Test
     void test2() {
-        String expression = "[}";
-        boolean valid = new ParenthesesValidator().isValid(expression);
+        boolean valid = new ParenthesesValidator().isValid("[}");
         assert !valid;
         System.out.println(valid);
     }
 
     @Test
     void test3() {
-        String expression = "[()(){}]";
-        boolean valid = new ParenthesesValidator().isValid(expression);
+        boolean valid = new ParenthesesValidator().isValid("[()(){}]");
         assert valid;
         System.out.println(valid);
     }
 
     @Test
     void test4() {
-        String expression = "[{()}{()}]";
-        boolean valid = new ParenthesesValidator().isValid(expression);
+        boolean valid = new ParenthesesValidator().isValid("[{()}{()}]");
         assert valid;
         System.out.println(valid);
     }
 
     @Test
     void test5() {
-        String expression = "[(({})]";
-        boolean valid = new ParenthesesValidator().isValid(expression);
+        boolean valid = new ParenthesesValidator().isValid("[(({})]");
         assert !valid;
         System.out.println(valid);
     }
 
     @Test
     void test6() {
-        String expression = "[({)}]";
-        boolean valid = new ParenthesesValidator().isValid(expression);
+        boolean valid = new ParenthesesValidator().isValid("[({)}]");
+        assert !valid;
+        System.out.println(valid);
+    }
+
+    @Test
+    void test7() {
+        boolean valid = new ParenthesesValidator().isValid("");
+        assert valid;
+        System.out.println(valid);
+    }
+
+    @Test
+    void test8() {
+        boolean valid = new ParenthesesValidator().isValid("[({)");
         assert !valid;
         System.out.println(valid);
     }
