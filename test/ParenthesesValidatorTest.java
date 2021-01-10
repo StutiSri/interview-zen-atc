9498f6eebee8f12ead4e76c5ce5e9e9ab0822f28
Stuti Srivastava <stutisrivastv@gmail.com>
2021-01-10 20:41:31 +0530

    Parentheses Validator - #3

--- test/ParenthesesValidatorTest.java
+++ test/ParenthesesValidatorTest.java
@@ -1,5 +1,11 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ParenthesesValidatorTest {
-
+    @Test
+    void test1() {
+        String expression = "[]";
+        System.out.println(new ParenthesesValidator().isValid(expression));
+    }
 }
